f4bae1b4df042d2a4ee1e53156b027ae0a7990af
gallula david <82768589+dgallula@users.noreply.github.com>
2021-12-28 08:54:27 +0000

    ajout de regex

--- script.js
+++ script.js
@@ -25,3 +25,94 @@ var area;
     }
 
     window.onload = init;
+
+
+      //  regex 
+
+    var validation = document.getElementById("Draw")
+    var xAxios = document.getElementById("x")
+    var yAyios = document.getElementById("y")
+    var h =  document.getElementById("L")
+    var w = document.getElementById("l")
+
+
+     var xAxios_m = document.getElementById("error-xAxios")
+     var xAxios_v = /^[0-9]/;
+
+     
+     var yAyios_m = document.getElementById("error-yAxios")
+     var yAyios_v = /^[0-9]/;
+
+     
+     var h_m = document.getElementById("error-yAxios")
+     var h_v = /^[0-9]/;
+
+    
+     
+     var w_m = document.getElementById("error-yAxios")
+     var w_v = /^[0-9]/;
+ 
+   
+function f_valid(e) {
+  console.log("erreur")
+
+  //  pour xAxios
+
+   if (xAxios.validity.valueMissing) {
+     e.preventDefault();
+     xAxios_m.innerText = 'Prenom manquant';
+     prenom_m.style.color = 'red';
+  } else if (prenom_v.test(xAxios.value )== false ) {
+    event.preventDefault();
+    xAxios_m.textContent = "Format incorrect";
+    xAxios_m.style.color = "orange"
+  } else {
+
+  }
+
+  // pour yAyios 
+
+  
+  if (yAyios.validity.valueMissing) {
+    e.preventDefault();
+    yAyios_m.innerText = 'Prenom manquant';
+    yAyios_m.style.color = 'red';
+ } else if (prenom_v.test(yAyios.value )== false ) {
+   event.preventDefault();
+   yAyios_m.textContent = "Format incorrect";
+   yAyios_m.style.color = "orange"
+ } else {
+
+ }
+
+ // pour h 
+
+  
+ if (h.validity.valueMissing) {
+  e.preventDefault();
+  h_m.innerText = 'Prenom manquant';
+  h_m.style.color = 'red';
+} else if (prenom_v.test(h.value )== false ) {
+ event.preventDefault();
+ h_m.textContent = "Format incorrect";
+ h_m.style.color = "orange"
+} else {
+
+}
+
+//pour  w 
+
+
+if (w.validity.valueMissing) {
+  e.preventDefault();
+  w_m.innerText = 'Prenom manquant';
+  w_m.style.color = 'red';
+} else if (prenom_v.test(w.value )== false ) {
+ event.preventDefault();
+ w_m.textContent = "Format incorrect";
+ w_m.style.color = "orange"
+} else {
+
+   }
+
+} 
